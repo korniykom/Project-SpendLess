dfb62ce4d049a40c7c857f7c4f87e11cb5029ff7
Max <korniykom@gmail.com>
2025-07-09 12:14:44 +0300

    feat: add Color Scheme to the Project

--- app/src/main/java/com/korniykom/spendless/theme/Color.kt
+++ app/src/main/java/com/korniykom/spendless/theme/Color.kt
@@ -9,3 +9,39 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Primary = Color(0xFF5A00C8)
+val OnPrimary = Color(0xFFFFFFFF)
+val PrimaryContainer = Color(0xFF8138FF)
+val PrimaryFixed = Color(0xFFEADDFF)
+val OnPrimaryFixed = Color(0xFF24005A)
+val OnPrimaryFixedVariant = Color(0xFF5900C7)
+val InversePrimary = Color(0xFFD2BCFF)
+
+val Secondary = Color(0xFF5F6200)
+val SecondaryContainer = Color(0xFFD2E750)
+val OnSecondaryContainer = Color(0xFF414300)
+val SecondaryFixed = Color(0xFFE5EA58)
+val SecondaryFixedDim = Color(0xFFC9CE3E)
+
+val TertiaryContainer = Color(0xFFC4E0F9)
+
+val Error = Color(0xFFA40019)
+val OnError = Color(0xFFFFFFFF)
+val Success = Color(0xFF29AC08)
+
+val Surface = Color(0xFFFCF9F9)
+val OnSurface = Color(0xFF1B1B1C)
+val OnSurfaceVariant = Color(0xFF44474B)
+val Outline = Color(0xFF75777B)
+val SurfaceContainerLowest = Color(0xFFFFFFFF)
+val SurfaceContainerLow = Color(0xFFF6F3F3)
+val SurfaceContainer = Color(0xFFF0EDED)
+val SurfaceContainerHighest = Color(0xFFE4E2E2)
+
+val InverseSurface = Color(0xFF303031)
+val InverseOnSurface = Color(0xFFF3F0F0)
+val Background = Color(0xFFFEF7FF)
+val OnBackground = Color(0xFF1D1A25)
+
+
--- app/src/main/java/com/korniykom/spendless/theme/Theme.kt
+++ app/src/main/java/com/korniykom/spendless/theme/Theme.kt
@@ -10,48 +10,39 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import com.korniykom.spendless.SpendLessApp
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val spendLessColorScheme = lightColorScheme(
+    primary = Primary,
+    onPrimary = OnPrimary,
+    primaryContainer = PrimaryContainer,
+    inversePrimary = InversePrimary,
+    secondary = Secondary,
+    secondaryContainer = SecondaryContainer,
+    onSecondaryContainer = OnSecondaryContainer,
+    tertiaryContainer = TertiaryContainer,
+    error = Error,
+    onError = OnError,
+    surface = Surface,
+    surfaceContainerLowest = SurfaceContainerLowest,
+    surfaceContainerLow = SurfaceContainerLow,
+    surfaceContainer = SurfaceContainer,
+    surfaceContainerHighest = SurfaceContainerHighest,
+    onSurface = OnSurface,
+    onSurfaceVariant = OnSurfaceVariant,
+    outline = Outline,
+    inverseSurface = InverseSurface,
+    inverseOnSurface = InverseOnSurface,
+    background = Background,
+    onBackground = OnBackground
 )
 
 @Composable
 fun SpendLessTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = spendLessColorScheme,
         typography = Typography,
         content = content
     )
